d9347048a3446cd9643a2b1cd1f7f27ba16db4b7
Paul Curry <pcurry@gmail.com>
2022-10-10 12:46:11 -0400

    Add a second paragraph to more closely follow the example code

--- blog-generator/hello.hs
+++ blog-generator/hello.hs
@@ -9,6 +9,9 @@ main = putStrLn (render myHtml)
 myHtml :: Html
 myHtml = html_
   "Hello World Title"
-  (append_ (h1_ "Hello, World") (p_ "Let's learn some Haskell!"))
-
-
+  ( append_
+    (h1_ "Hello, World")
+    ( append_
+      (p_ "Let's learn some Haskell! (which will probably read with a lisp)")
+      (p_ "And try to make a blog generator.")
+    ))
